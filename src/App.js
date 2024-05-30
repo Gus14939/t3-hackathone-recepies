3d800c6d29191ffada501928dbeec80a118a4acb
Gus14939 <148546545+Gus14939@users.noreply.github.com>
2024-05-30 18:42:42 +1000

    Feat: main structure setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
