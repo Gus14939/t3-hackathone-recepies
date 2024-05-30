6cc5f7332c7ce0b54c8627f040d7375288625afe
Gus14939 <148546545+Gus14939@users.noreply.github.com>
2024-05-30 18:56:56 +1000

    feat: Nav and Pages

--- src/App.js
+++ src/App.js
@@ -2,10 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
+import Home from './pages/Home';
+import Favourites from './pages/Favourites';
+import Details from './pages/Details';
 
 function App() {
   return (
     <div>
+    <div className='min-h-screen p-6 bg-white text-gray-600 text-lg'>
+      <Navbar />
+      <Routes>
+
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/favourites'
+          element={<Favourites />}
+        />
+        <Route
+          path='/recipe/:id'
+          element={<Details />}
+        />
+
+      </Routes>
+    </div>
+
     </div>
   );
 }
